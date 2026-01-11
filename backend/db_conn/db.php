67222b48c457b8e051b59e0359a72c1a113f81a1
onlineappointmentsystem00-spec <onlineappointmentsystem00@gmail.com>
2026-01-11 12:43:12 +0800

    Update db.php

--- backend/db_conn/db.php
+++ backend/db_conn/db.php
@@ -1,7 +1,26 @@
-<?php
+<!-- php
 $conn = mysqli_connect("127.0.0.1", "root", "@ABC12abc", "appointment_db");
 
 if (!$conn) {
     die("Database connection failed");
 }
+ 
+?> -->
+
+<?php
+$serverName = "(localdb)\\AppointmentDB";
+
+$connectionOptions = array(
+    "Database" => "LIVEAppointmenBookingtDB",
+    "TrustServerCertificate" => true
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+
 ?>
+
