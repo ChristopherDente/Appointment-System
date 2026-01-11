d21d19f76b9ce8a3e1bc6dca3267b5404fcf4166
onlineappointmentsystem00-spec <onlineappointmentsystem00@gmail.com>
2026-01-11 14:34:25 +0800

    Update dashboard.php

--- frontend/users/dashboard.php
+++ frontend/users/dashboard.php
@@ -21,119 +21,39 @@
                 <span class="navbar-brand fw-semibold">
                     <i class="bi bi-heart-pulse"></i> Online Appointment Booking System
                 </span>
-
             </a>
 
-            <!-- Mobile toggle -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <!-- Navbar links -->
             <div class="collapse navbar-collapse" id="mainNavbar">
                 <ul class="navbar-nav ms-auto align-items-lg-center">
                     <li class="nav-item">
-                        <a class="nav-link active" href="dashboard.php">
-                            Dashboard
-                        </a>
+                        <a class="nav-link active" href="dashboard.php">Dashboard</a>
                     </li>
-
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="appointmentDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Appointments
-                        </a>
+                            data-bs-toggle="dropdown" aria-expanded="false">Appointments</a>
                         <ul class="dropdown-menu" aria-labelledby="appointmentDropdown">
                             <li><a class="dropdown-item" href="book.php">Book Appointment</a></li>
-                            <li><a class="dropdown-item" href="upcoming.php">Upcoming
-                                    Appointments</a></li>
-                            <li><a class="dropdown-item" href="past.php">Past Appointments</a>
-                            </li>
-                            <li><a class="dropdown-item" href="history.php">Appointment
-                                    History</a></li>
+                            <li><a class="dropdown-item" href="upcoming.php">Upcoming Appointments</a></li>
+                            <li><a class="dropdown-item" href="past.php">Past Appointments</a></li>
+                            <li><a class="dropdown-item" href="history.php">Appointment History</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="payments.php">
-                            Payments
-                        </a>
+                        <a class="nav-link" href="payments.php">Payments</a>
                     </li>
-
-
                     <li class="nav-item">
-                        <a class="nav-link" href="support.php">
-                            Support
-                        </a>
+                        <a class="nav-link" href="support.php">Support</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link position-relative d-flex align-items-center justify-content-center dropdown-toggle"
-                            href="#" id="notificationDropdown" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" style="width: 40px; height: 40px;">
-
-                            <i class="bi bi-bell fs-5 text-white"></i>
-
-                            <span id="notificationBadge" class="notification-count">
-                                3
-                            </span>
-                        </a>
-
-                        <ul class="dropdown-menu dropdown-menu-end shadow notification-dropdown"
-                            aria-labelledby="notificationDropdown">
-
-                            <li class="dropdown-header fw-semibold">
-                                Notifications
-                            </li>
-
-                            <li>
-                                <a class="dropdown-item d-flex gap-2" href="#">
-                                    <i class="bi bi-check-circle text-success"></i>
-                                    Appointment Confirmation
-                                </a>
-                            </li>
-
-                            <li>
-                                <a class="dropdown-item d-flex gap-2" href="#">
-                                    <i class="bi bi-alarm text-primary"></i>
-                                    Appointment Reminders
-                                </a>
-                            </li>
-
-                            <li>
-                                <a class="dropdown-item d-flex gap-2" href="#">
-                                    <i class="bi bi-x-circle text-danger"></i>
-                                    Cancellation Updates
-                                </a>
-                            </li>
-
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-
-                            <li>
-                                <a class="dropdown-item text-center text-primary fw-semibold"
-                                    href="frontend/notifications.php">
-                                    View all notifications
-                                </a>
-                            </li>
-                        </ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Profile
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="profileDropdown">
-                            <li><a class="dropdown-item" href="view.php">View Profile</a></li>
-                            <li><a class="dropdown-item" href="edit.php">Edit Profile</a></li>
-                            <li><a class="dropdown-item" href="change-password.php">Change Password</a>
-                            </li>
-                            <li><a class="dropdown-item" href="medical-info.php">Medical
-                                    Information</a></li>
-                        </ul>
-                    </li>
-
+                    
                     <li class="nav-item ms-lg-3">
-                        <a class="nav-link-login nav-link" href="frontend/logout.php"
+                        <a class="nav-link-login nav-link" href="logout.php"
                             onclick="return confirm('Are you sure you want to logout?');">
                             <i class="bi bi-box-arrow-right"></i> Logout
                         </a>
@@ -352,7 +272,7 @@
                         <li><a href="history.php">Appointment History</a></li>
                         <li><a href="payments.php">Payments</a></li>
                         <li><a href="support.php">Support</a></li>
-                        <li><a href="view.php">Profile</a></li>
+                        <li><a href="profile.php">Profile</a></li>
                     </ul>
                 </div>
                 <div class="col-md-4">
