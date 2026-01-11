982ec8bbbdb9dcc789097614312a4c2c3b495ea7
onlineappointmentsystem00-spec <onlineappointmentsystem00@gmail.com>
2026-01-11 10:25:39 +0800

    Update book.php

--- frontend/users/book.php
+++ frontend/users/book.php
@@ -291,6 +291,7 @@
                             </div>
                         </div>
 
+                        
                         <!-- Navigation Buttons -->
                         <div class="d-flex justify-content-between mt-4 pt-3 border-top">
                             <button type="button" class="btn btn-outline-doctor" id="prevBtn" onclick="changeStep(-1)"
