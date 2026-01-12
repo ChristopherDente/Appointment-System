932b1d921ce5ddcf18aabdf975aa826e210b4f2f
Swaire <144995004+Swaire@users.noreply.github.com>
2026-01-12 09:42:21 +0800

    db connection if else

--- backend/db_conn/db.php
+++ backend/db_conn/db.php
@@ -1,26 +1,38 @@
-<!-- php
-$conn = mysqli_connect("127.0.0.1", "root", "@ABC12abc", "appointment_db");
-
-if (!$conn) {
-    die("Database connection failed");
-}
- 
-?> -->
-
 <?php
-$serverName = "(localdb)\\AppointmentDB";
 
-$connectionOptions = array(
-    "Database" => "LIVEAppointmenBookingtDB",
-    "TrustServerCertificate" => true
-);
+// CHANGE THIS ONLY
+$DB_DRIVER = 'sqlsrv'; // 'mysql' or 'sqlsrv'
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+if ($DB_DRIVER === 'mysql') {
 
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
+    $conn = mysqli_connect(
+        "127.0.0.1",
+        "root",
+        "@ABC12abc",
+        "appointment_db"
+    );
+
+    if (!$conn) {
+        die("MySQL connection failed: " . mysqli_connect_error());
+    }
+
 }
 
+if ($DB_DRIVER === 'sqlsrv') {
+
+    $serverName = "(localdb)\\AppointmentDB";
+
+    $connectionOptions = array(
+        "Database" => "LIVEAppointmenBookingtDB",
+        "TrustServerCertificate" => true
+    );
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+    if ($conn === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+}
 
 ?>
-
