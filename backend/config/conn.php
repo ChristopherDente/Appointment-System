f4213291b3237dcd2c5a28867be5ceffb84ca80c
Swaire <144995004+Swaire@users.noreply.github.com>
2026-01-16 14:58:25 +0800

    login

--- backend/config/conn.php
+++ backend/config/conn.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username   = "root";
-$password   = "";
+$password   = "@ABC12abc";
 $dbname     = "appoitmentsystem";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
